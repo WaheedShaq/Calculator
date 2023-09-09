b7424b0233990102ba8163dfc75d31064a8a59ee
WaheedShaq <waheedshaqra@gmail.com>
2023-09-09 08:28:05 +0300

    commiting to save progress so far, gonna redo the entire logic and html

--- script.js
+++ script.js
@@ -3,49 +3,100 @@ let divideNumber = document.getElementById('#÷');
 let addNumber = document.getElementById('#+');
 let subtractNumber = document.getElementById('#-');
 let multiplyNumber = document.getElementById('#*');
-let operator = document.getElementById('#operator');
-let number = document.getElementById('#numbers');
+let clear = document.getElementById('clear');
+clear.addEventListener('click', clearScreen);
+let operators = document.querySelectorAll('#operator');
+let equal = document.querySelector('#equal');
+equal.addEventListener('click', calculate);
+let number = document.querySelectorAll('#numbers');
+let buttons = document.querySelectorAll('.calc-button');
 
-let total = 0;
-let num1 = 5;
-let num2 = 5;
+let currentNumber = '';
+let previousNumber = '';
+let operator = '';
 
-function add(num1, num2) {
-  return num1 + num2;
+function clearScreen() {
+  screen.textContent = '';
 }
 
-function subtract(num1, num2) {
-  return num1 - num2;
-}
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    handleNumber(e.target.textContent);
+  });
+});
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
+operators.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(e.target.textContent);
+  });
+});
 
-function divide(num1, num2) {
-  return num1 / num2;
-}
-
-function calculation(num1, num2, operator) {
-  if (operator.innerHTML === '+') {
-    add(num1, num2);
-  }
-
-  if (operator.innerHTML === '-') {
-    subtract(num1, num2);
-  }
-
-  if (operator.innerHTML === '*') {
-    multiply(num1, num2);
-  }
-
-  if (operator.innerHTML === '÷') {
-    divide(num1, num2);
+function handleNumber(number) {
+  if (currentNumber.length <= 100) {
+    currentNumber += number;
+    screen.textContent = currentNumber;
   }
 }
 
-// function testScreen() {
+function calculate() {
+  previousNumber = Number(previousNumber);
+  currentNumber = Number(currentNumber);
+
+  if (operator === '+') {
+    previousNumber = previousNumber + currentNumber;
+  } else if (operator === '-') {
+    previousNumber = previousNumber - currentNumber;
+  } else if (operator === '*') {
+    previousNumber = previousNumber * currentNumber;
+  } else if (operator === '÷') {
+    previousNumber = previousNumber / currentNumber;
+  }
+
+  screen.innerHTML = previousNumber;
+}
+
+// function add(num1, num2) {
+//   return num1 + num2;
+// }
+
+// function subtract(num1, num2) {
+//   return num1 - num2;
+// }
+
+// function multiply(num1, num2) {
+//   return num1 * num2;
+// }
+
+// function divide(num1, num2) {
+//   return num1 / num2;
+// }
+
+// operator.forEach((btn) => {
+//   btn.addEventListener('click', (e) => {
+//     handleOperator(e.target.textContent);
+//   });
+// });
+
+// function calculation(num1, num2, operator) {
+//   if (operator.innerHTML === '+') {
+//     screen.innerHTML = add(num1, num2);
+//   }
+
+//   if (operator.innerHTML === '-') {
+//     screen.innerHTML = subtract(num1, num2);
+//   }
+
+//   if (operator.innerHTML === '*') {
+//     screen.innerHTML = multiply(num1, num2);
+//   }
+
+//   if (operator.innerHTML === '÷') {
+//     screen.innerHTML = divide(num1, num2);
+//   }
+// }
+
+// function screenResult() {
 //   screen.innerHTML = add(num1, num2);
 // }
 
-// testScreen();
+// screenResult();
