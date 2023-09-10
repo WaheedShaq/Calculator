1a0ec4dc3177b62d45057cbdb3ac6e19c7e8b236
WaheedShaq <waheedshaqra@gmail.com>
2023-09-10 07:15:10 +0300

    pushing before making changes to save a snapshot of this code

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let result = '';
 let operator = '';
 
 function calculate() {
-  console.log({ num1, num2, operator });
   num1 = Number(num1);
   num2 = Number(num2);
+  console.log({ num1, num2, operator });
 
   if (operator === '+') {
     result = Number(num1) + Number(num2);
@@ -25,22 +25,21 @@ function calculate() {
     result = Number(num1) / Number(num2);
   }
 
-  console.log(result);
+  screen.textContent = result;
+  console.log(` Result is: ${result}`);
 }
-
+// Clears calculator screen
 function clearScreen() {
   num1 = '';
   num2 = '';
   operator = '';
   screen.textContent = '0';
 }
-
+// Generate number 1 and an operator
 buttons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     if (btn.textContent >= 0 && btn.textContent <= 9) {
       console.log(handleNumberOne(e.target.textContent));
-
-      console.log(handleNumberTwo(e.target.textContent));
 
       if (
         btn.textContent === '+' ||
@@ -48,35 +47,54 @@ buttons.forEach((btn) => {
         btn.textContent === '÷' ||
         btn.textContent === '*'
       ) {
-        console.log((operator = btn.textContent));
+        console.log(`Operator is${(operator = btn.textContent)}`);
       }
     }
   });
 });
+// Generate number 2
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    if (btn.textContent >= 0 && btn.textContent <= 9) {
+      console.log(handleNumberTwo(e.target.textContent));
+    }
+  });
+});
 
+// Select operator
 operators.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     operator = e.target.textContent;
+    screen.textContent = operator;
   });
 });
 
 function handleNumberOne(number) {
-  console.log(`handleNumber was called, input was ${num1} and ${number}`);
+  console.log(`handleNumberOne was called, input was ${num1} and ${number}`);
   if (num1.length <= 100) {
     num1 += Number(number);
-    screen.textContent = num1;
+    return (screen.textContent = num1);
   }
-  console.log(`output for handleOne was ${num1}`);
+  console.log(`output for handleNumberOne was ${num1}`);
+
+  // return num1;
 }
 
 function handleNumberTwo(number) {
   console.log(`handleNumberTwo was called, input was ${num2} and ${number}`);
   if (num2.length <= 100) {
     num2 += Number(number);
-    screen.textContent = num2;
+    return (screen.textContent = num2);
   }
-  console.log(`output for handleTwo was ${num2}`);
+  console.log(`output for handleNumberTwo was ${num2}`);
+  // return num2;
 }
+
+// function handleOperator(op) {
+//   operator = op;
+//   screen.textContent = num1 + ' ' + operator;
+//   num1 = '';
+// }
 
 equal.addEventListener('click', calculate);
 clear.addEventListener('click', clearScreen);
